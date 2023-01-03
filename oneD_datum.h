6a25bbe6c83efde46e8a77f3ea073311edcfc1db
joy <jjooyy1872@gmail.com>
2023-01-03 18:40:34 +0800

    Initial commit

---
+++ oneD_datum.h
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+////////////////------------------------宣告-----------------//////////////
+
+class oneD_datum{//一維數據處裡
+    protected:
+        vector<int> x;
+        double average; //算術平均
+        double standard_deviation; //標準差
+
+    public:
+        double ave(){ //average.getter
+            return average;
+        }//ave()
+
+        double percentile(int pt); //百分位數(中位數) //實作在.cpp
+
+        double percentile_range(int pt1,int pt2){ //百分、四分位距
+            if(pt1 > pt2) swap(pt1,pt2);
+            return percentile(pt2) - percentile(pt1);
+        }//percentile_range()
+
+        void print();//print() //實作在.cpp
+        
+        void setup(int n);//setup() //實作在.cpp
+        
+        oneD_datum(int n){//constructor
+            setup(n);
+            print();
+        }//constructor
+
+        
+
+}; //oneD_datum
---
+++ project_presentation.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+#include"oneD_datum.h"
+using namespace std;
+
+///////////////////////--------------------實作--------------------///////////////////////////////////
+double oneD_datum::percentile(int pt) {
+    double index = pt * x.size() * 0.01;
+    if (index == x.size()) return x[x.size() -1]; //P100  = an 
+    if (index != (int)index) return x[(int)index];  //index = 2.5  --> a3 --> x[2]
+    else{ 
+        double temp1 = x[(int)index-1] + x[(int)index]; //index = 2 --> (a2+a3)/2 --> (x[1]+x[2]) /2
+        return temp1 / 2; 
+    }
+}//percentile()
+
+void oneD_datum::print(){
+    cout << "current data: ";
+    for(int i = 0; i < x.size(); i++){
+        cout << x[i] << " ";
+    }
+    cout << "\n";
+    cout << "current average: " << average<< "\n";
+    cout << "current median: " << percentile(50)<<"\n";
+    cout << "current standard deviation: " << standard_deviation << "\n";
+    return;
+}//print()
+
+void oneD_datum::setup(int n){
+    int temp;
+    for(int i = 0; i < n; i++){
+        cin >> temp;
+        x.push_back(temp);
+    }
+    double temp2 = 0;
+    sort(x.begin(), x.end(), less<int>());
+    for(auto i:x){
+        temp2 += i;
+    }
+    average = temp2 / x.size();
+    double tempsd = 0;
+    for(auto i:x){
+        tempsd += (i-average)*(i-average);
+    }
+    standard_deviation = sqrt(tempsd / x.size());
+    return;
+}//setup()
+
+
+////////////////////////--------------------執行--------------------//////////////////////////////////
+int main(){
+    int n; 
+    cin >> n;                               //sample Input: 5
+    oneD_datum A(n);                        //sample Input: 6 5 4 3 1 
+                                            //Ouput: current data: 1 3 4 5 6
+                                            //       current average: 3.8
+                                            //       current median: 4
+                                            //       current standard deviation: 1.72047
+    A.setup(1);                             //sample Input: 2
+    A.print();                              //Ouput: current data: 1 2 3 4 5 6
+                                            //       current average: 3.5
+                                            //       current median: 3.5
+                                            //       current standard deviation: 1.70783
+    cout << "Q3 - Q1 = "<< A.percentile_range(75,25);      //= Q3 - Q1 = a5 - a2 = 3 
+
+    //還可以用
+    //.ave() 回傳平均值
+    //.percentile(int pt) 回傳百分位數Ppt
+}
